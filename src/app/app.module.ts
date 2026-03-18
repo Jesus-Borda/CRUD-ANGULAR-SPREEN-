e71b6b279da085eaa429bc476c9305b38426859d
Jesus Borda <jesusdeveloper2314@gmail.com>
2026-03-17 23:12:25 -0500

    Creacion de Rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,29 @@ import { CustomerListComponent } from './components/customer-list/customer-list.
 import { HttpClientModule } from '@angular/common/http';
 import { CustomerAddComponent } from './components/customer-add/customer-add.component';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes : Routes = [
+  {path: '', component: CustomerListComponent},//http://localhost:4200 -> CustomerListComponent
+  // En este caso, se define una ruta con el path 'customers' que se asocia con el componente CustomerListComponent.
+  // Esto significa que cuando el usuario navegue a la URL '/customers', se mostrará el componente CustomerListComponent.
+  {path: 'customers/add', component: CustomerAddComponent}//http://localhost:4200/customers/add -> CustomerAddComponent
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     EjemploComponent,
     CustomerListComponent,
-    CustomerAddComponent
+    CustomerAddComponent 
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
-  bootstrap: [CustomerAddComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
